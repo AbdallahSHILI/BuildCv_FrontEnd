62d5571bd67070413b630346e411bbbec776a8ba
Abdallah Shili <shiliabdallah1234@gmail.com>
2025-10-22 14:32:25 +0100

    Update assets import structure and replace external icons with local assets

---
+++ src/assets/index.js
@@ -0,0 +1,5 @@
+import facebookIcon from "./facebookIcon.svg";
+import googleIcon from "./GoogleIcon.svg";
+import rocket from "./rocket.png";
+
+export { facebookIcon, googleIcon, rocket };
--- src/components/auth/signupForm/signupForm.jsx
+++ src/components/auth/signupForm/signupForm.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import styles from "../AuthLayout.module.css";
 import { AUTH_ENDPOINTS } from "../../../config/api";
+import { googleIcon, facebookIcon } from "../../../assets/index";
 
 const SignupForm = () => {
   const [name, setName] = useState("");
@@ -117,20 +118,19 @@ const SignupForm = () => {
           onClick={handleGoogleSignup}
           type="button"
         >
-          <img
-            src="https://cdn.cdnlogo.com/logos/g/35/google-icon.svg"
-            alt="Google"
-            className={styles.socialIcon}
-          />
+          <img src={googleIcon} alt="Google" className={styles.socialIcon} />
         </button>
+
         <button
           className={styles.socialBtn}
           onClick={handleFacebookSignup}
           type="button"
         >
-          <svg className={styles.socialIcon} viewBox="0 0 24 24" fill="#1877F2">
-            <path d="M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.47h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.47h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z" />
-          </svg>
+          <img
+            src={facebookIcon}
+            alt="Facebook"
+            className={styles.socialIcon}
+          />
         </button>
       </div>
     </div>
