1e1ee4573693f6551c7a2d1993593ba1d43722d0
Abdallah Shili <shiliabdallah1234@gmail.com>
2025-10-22 11:56:57 +0100

    Add framer-motion for animations; implement animated routes and enhance AuthLayout with slide-in effects

--- src/App.js
+++ src/App.js
@@ -4,18 +4,30 @@ import {
   Routes,
   Route,
   Navigate,
+  useLocation,
 } from "react-router-dom";
+import { AnimatePresence } from "framer-motion";
 import Login from "./components/auth/Login";
 import Signup from "./components/auth/Signup";
 
-function App() {
+function AnimatedRoutes() {
+  const location = useLocation();
+
   return (
-    <Router>
-      <Routes>
+    <AnimatePresence mode="wait">
+      <Routes location={location} key={location.pathname}>
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/" element={<Navigate to="/login" replace />} />
       </Routes>
+    </AnimatePresence>
+  );
+}
+
+function App() {
+  return (
+    <Router>
+      <AnimatedRoutes />
     </Router>
   );
 }
