dc99f4ba1bdb94a9581b6fc6218c317e00eeb393
Abdallah Shili <shiliabdallah1234@gmail.com>
2026-07-11 12:38:31 +0100

    Add EntryEditor components including header, footer, text fields, and icons

---
+++ src/pages/content/components/SummaryCard/EntryEditor/EditEntryFooter.jsx
@@ -0,0 +1,15 @@
+import { CheckIcon } from "./icons";
+import styles from "./EditEntryFooter.module.css";
+
+// Sticky footer with the gradient "Done" button, shared by every
+// edit-entry panel.
+export default function EditEntryFooter({ onDone, label = "Done" }) {
+  return (
+    <div className={styles.footer}>
+      <button className={styles.doneBtn} onClick={onDone} type="button">
+        <CheckIcon />
+        {label}
+      </button>
+    </div>
+  );
+}
